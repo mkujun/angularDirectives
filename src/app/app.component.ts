2ac63d431e8525ce28ed81a8e44c36963807f5ed
= <=>
2022-03-28 19:37:03 +0200

    angular directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .greater {
+      color: red;
+    },
+    .lesser {
+      color: black;
+    }
+  `]
 })
 export class AppComponent {
   title = 'angular_directives';
+
+  showHideButton = true;
+  count: number = 0;
+  clicks: Array<number> = [];
+
+  showHide() {
+    this.showHideButton = !this.showHideButton;
+    this.count = this.count + 1;
+    this.clicks.push(this.count);
+  }
+
+  getColor(item: number) {
+    return item > 5 ? 'blue': '';
+  }
 }
